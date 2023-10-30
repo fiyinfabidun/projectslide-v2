413859ac40bfa1a9b4f2a4c575e6125375d13f4e
fiyinfoluwa <112215226+fiyinfabidun@users.noreply.github.com>
2023-10-30 12:02:44 +0100

    Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,9 +25,12 @@ function Footer() {
             <Link to='/phoenix'>phoenix</Link>
           </div>
           <div className="footer-media">
+                <a href="https://instagram.com/f.iyin__?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D">
+                 <img src={insta} alt="" />
+                </a>
             <img src={whatsapp} alt="" />
             <img src={x} alt="" />
-            <img src={insta} alt="" />
+          
 
           </div>
           <p style={{
