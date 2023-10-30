7821c32acfa75eb23f34a8ab73573a25f6d1c064
fiyinfoluwa <112215226+fiyinfabidun@users.noreply.github.com>
2023-10-30 09:36:49 +0100

    Aspectum.js

--- src/Pages/Aspectum.js
+++ src/Pages/Aspectum.js
@@ -9,12 +9,58 @@ import Footer from '../components/Footer';
 
 
 function Aspectum() {
+
+    const CLINICAL_CHEM=[
+    { file: `${lesson1}`, filename: 'introductory to microbiology', label: 'introductory tomicrobiology' },
+    { file: `${lesson1}`, filename: 'Link 2', label: 'Link 2' },
+    { file: `${lesson1}`, filename: 'Link 3', label: 'Link 3' },
+    { file: `${lesson1}`, filename: 'Link 4', label: 'Link 4' },
+  ];
+  const HEAMAT=[
+    { file: `${lesson1}`, filename: 'Link 2', label: 'Link 2' },
+    { file: `${lesson1}`, filename: 'Link 3', label: 'Link 3' },
+    { file: `${lesson1}`, filename: 'Link 4', label: 'Link 4' },
+]
+const HISTO=[
+  { file: `${lesson1}`, filename: 'Link 2', label: 'Link 2' },
+  { file: `${lesson1}`, filename: 'Link 3', label: 'Link 3' },
+  { file: `${lesson1}`, filename: 'Link 4', label: 'Link 4' },
+]
+const MICRO=[
+  { file: `${lesson1}`, filename: 'Link 2', label: 'Link 2' },
+  { file: `${lesson1}`, filename: 'Link 3', label: 'Link 3' },
+  { file: `${lesson1}`, filename: 'Link 4', label: 'Link 4' },
+]
+const PHARMAC=[
+  { file: `${lesson1}`, filename: 'Origin of pharmacology-prof olayemi', label: 'Origin of pharmacology' },
+  { file: `${lesson1}`, filename: 'Pharmacokinetics and structure-activity relationship of drugs', label: 'Pharmacokinetics and structure-activity' },
+  { file: `${lesson1}`, filename: 'Link 4', label: 'Link 4' },
+]
   return (
-    <div>
+    <>
+     <NavBar/>
+    <HeroBakground 
+    title='Aspectum'
+    backgroundImage={aspectum}
+    />
+    <div className="class-body">
+    {/* <a href={lesson1} download='lesson1' className='link'>mls305</a> */}
+    <h2 className='vault-text'>Welcome Aspectum, what are you getting from the vault?</h2>
+    <h2 className='header'>Vault items available</h2>
+    <div className="courses-available">
+    <Test label="Basic clinical chemistry- 311" links={CLINICAL_CHEM} downloadable={true}/>
+    <Test label="Basic Hematology- 312" links={HEAMAT} downloadable={true}/>
+    <Test label="Basic Microbiology- 313" links={HISTO} downloadable={true}/>
+    <Test label="Basic histopathology- 314" links={MICRO} downloadable={true}/>
+    <Test label="General Biochemistry- 316" links={MICRO} downloadable={true}/>
+    <Test label="Basic pharmac thera-317" links={PHARMAC} downloadable={true}/>
+
+   </div>
     
+    </div>
     <Footer/>
 
-    </div>
+    </>
   )
 }
 
