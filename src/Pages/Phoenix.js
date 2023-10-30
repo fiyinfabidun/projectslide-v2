f9aca7f16b862d6623e5d8f6b1cb5ba0f0164c7e
fiyinfoluwa <112215226+fiyinfabidun@users.noreply.github.com>
2023-10-30 10:03:40 +0100

    Phoenix.js

--- src/Pages/Phoenix.js
+++ src/Pages/Phoenix.js
@@ -1,11 +1,68 @@
-import React from 'react'
-import Footer from '../components/Footer'
+import React from 'react';
+import NavBar from '../components/NavBar';
+import HeroBakground from '../components/HeroBakground';
+import pnx from '../assets/phoenix.jpg'
+// import PhronesisSlides from './PhroSLides';
+import lesson1 from '../assets/mls.pdf'
+import Test from './test';
+import Footer from '../components/Footer';
+
+
 
 function Phoenix() {
+  const CLINICAL_CHEM=[
+    { file: `${lesson1}`, filename: 'introductory to microbiology', label: 'introductory tomicrobiology' },
+    { file: `${lesson1}`, filename: 'Link 2', label: 'Link 2' },
+    { file: `${lesson1}`, filename: 'Link 3', label: 'Link 3' },
+    { file: `${lesson1}`, filename: 'Link 4', label: 'Link 4' },
+  ];
+  const HEAMAT=[
+    { file: `${lesson1}`, filename: 'Link 2', label: 'Link 2' },
+    { file: `${lesson1}`, filename: 'Link 3', label: 'Link 3' },
+    { file: `${lesson1}`, filename: 'Link 4', label: 'Link 4' },
+]
+const HISTO=[
+  { file: `${lesson1}`, filename: 'Link 2', label: 'Link 2' },
+  { file: `${lesson1}`, filename: 'Link 3', label: 'Link 3' },
+  { file: `${lesson1}`, filename: 'Link 4', label: 'Link 4' },
+]
+const MICRO=[
+  { file: `${lesson1}`, filename: 'Link 2', label: 'Link 2' },
+  { file: `${lesson1}`, filename: 'Link 3', label: 'Link 3' },
+  { file: `${lesson1}`, filename: 'Link 4', label: 'Link 4' },
+]
+const PHARMAC=[
+  { file: `${lesson1}`, filename: 'Origin of pharmacology-prof olayemi', label: 'Origin of pharmacology' },
+  { file: `${lesson1}`, filename: 'Pharmacokinetics and structure-activity relationship of drugs', label: 'Pharmacokinetics and structure-activity' },
+  { file: `${lesson1}`, filename: 'Link 4', label: 'Link 4' },
+]
   return (
-    <div>Phoenix<Footer/>
 
+    <>
+    <NavBar/>
+    <HeroBakground 
+    title='Phoenix'
+    backgroundImage={pnx}
+    />
+    <div className="class-body">
+    {/* <a href={lesson1} download='lesson1' className='link'>mls305</a> */}
+    <h2 className='vault-text'>Welcome Phoenix, what are you getting from the vault?</h2>
+    <h2 className='header'>Vault items available</h2>
+    <div className="courses-available">
+    <Test label="Basic clinical chemistry- 311" links={CLINICAL_CHEM} downloadable={true}/>
+    <Test label="Basic Hematology- 312" links={HEAMAT} downloadable={true}/>
+    <Test label="Basic Microbiology- 313" links={HISTO} downloadable={true}/>
+    <Test label="Basic histopathology- 314" links={MICRO} downloadable={true}/>
+    <Test label="General Biochemistry- 316" links={MICRO} downloadable={true}/>
+    <Test label="Basic pharmac thera-317" links={PHARMAC} downloadable={true}/>
+
+   </div>
+    
     </div>
+   
+    <Footer/>
+     </>
+    
   )
 }
 
