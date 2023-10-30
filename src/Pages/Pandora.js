a2caf18f3f3043880f86b8e760fd5af0e6478773
fiyinfoluwa <112215226+fiyinfabidun@users.noreply.github.com>
2023-10-30 02:26:40 +0100

    Pandora.js

--- src/Pages/Pandora.js
+++ src/Pages/Pandora.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Footer from '../components/Footer';
-import pan from '../assets/delph.jpg';
+import pandora from '../assets/pandora-bg.jpg';
 import NavBar from '../components/NavBar';
 import HeroBakground from '../components/HeroBakground';
 
