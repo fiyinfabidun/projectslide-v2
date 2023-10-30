d1e91d43c6f4bb5291b7500c89e1145df881c542
fiyinfoluwa <112215226+fiyinfabidun@users.noreply.github.com>
2023-10-30 09:34:02 +0100

    Update Aspectum.js

--- src/Pages/Aspectum.js
+++ src/Pages/Aspectum.js
@@ -1,9 +1,17 @@
-import React from 'react'
-import Footer from '../components/Footer'
+import React from 'react';
+import NavBar from '../components/NavBar';
+import HeroBakground from '../components/HeroBakground';
+import aspectum from '../assets/aspectum.jpeg'
+import lesson1 from '../assets/mls.pdf'
+import Test from './test';
+import Footer from '../components/Footer';
+
+
 
 function Aspectum() {
   return (
     <div>
+    
     <Footer/>
 
     </div>
