a6e31a0d9358d421a8828fcad6060e3c0abeab00
fiyinfoluwa <112215226+fiyinfabidun@users.noreply.github.com>
2023-11-05 23:35:12 +0100

    Genezens.js

--- src/Pages/Genezens.js
+++ src/Pages/Genezens.js
@@ -12,29 +12,18 @@ import Footer from '../components/Footer';
 function Genezens() {
   const CLINICAL_CHEM=[
     { file: `${lesson1}`, filename: 'introductory to microbiology', label: 'introductory tomicrobiology' },
-    { file: `${lesson1}`, filename: 'Link 2', label: 'Link 2' },
-    { file: `${lesson1}`, filename: 'Link 3', label: 'Link 3' },
-    { file: `${lesson1}`, filename: 'Link 4', label: 'Link 4' },
   ];
   const HEAMAT=[
-    { file: `${lesson1}`, filename: 'Link 2', label: 'Link 2' },
-    { file: `${lesson1}`, filename: 'Link 3', label: 'Link 3' },
-    { file: `${lesson1}`, filename: 'Link 4', label: 'Link 4' },
+    { file: `${lesson1}`, filename: 'Link 2', label: 'Link 2' }
 ]
 const HISTO=[
-  { file: `${lesson1}`, filename: 'Link 2', label: 'Link 2' },
-  { file: `${lesson1}`, filename: 'Link 3', label: 'Link 3' },
-  { file: `${lesson1}`, filename: 'Link 4', label: 'Link 4' },
+  { file: `${lesson1}`, filename: 'Link 2', label: 'Link 2' }
 ]
 const MICRO=[
-  { file: `${lesson1}`, filename: 'Link 2', label: 'Link 2' },
-  { file: `${lesson1}`, filename: 'Link 3', label: 'Link 3' },
-  { file: `${lesson1}`, filename: 'Link 4', label: 'Link 4' },
+  { file: `${lesson1}`, filename: 'Link 2', label: 'Link 2' }
 ]
 const PHARMAC=[
-  { file: `${lesson1}`, filename: 'Origin of pharmacology-prof olayemi', label: 'Origin of pharmacology' },
-  { file: `${lesson1}`, filename: 'Pharmacokinetics and structure-activity relationship of drugs', label: 'Pharmacokinetics and structure-activity' },
-  { file: `${lesson1}`, filename: 'Link 4', label: 'Link 4' },
+  { file: `${lesson1}`, filename: 'Origin of pharmacology-prof olayemi', label: 'Origin of pharmacology' }
 ]
   return (
 
@@ -49,18 +38,13 @@ const PHARMAC=[
     <h2 className='vault-text'>Welcome Genezens, what are you getting from the vault?</h2>
     <h2 className='header'>Vault items available</h2>
     <div className="courses-available">
-    <Test label="NSC 311: Basic Med/Surg" links={CLINICAL_CHEM} downloadable={true}/>
-    <Test label="NSC 312: Human Nutrition in Health & Disease " links={HEAMAT} downloadable={true}/>
-    <Test label="NSC 313: Human Behaviour in Health & Illness " links={HISTO} downloadable={true}/>
-    <Test label="NSC 315: General & Cellular Pathology" links={MICRO} downloadable={true}/>
-    <Test label="PHA 317: Basic Pharmacology/Therapeutics"links={MICRO} downloadable={true}/>
-    <Test label="MLS 333: Medical Microbiology" links={MICRO} downloadable={true}/>
-    <Test label="BTS 300: Basic Therapeutic Skills" links={MICRO} downloadable={true}/>
-    <Test label="PSY 234: Developmental Psychology" links={PHARMAC} downloadable={true}/>
-
+    <Test label="hematology" links={CLINICAL_CHEM} downloadable={true}/>
+    <Test label="morbid anatomy" links={HEAMAT} downloadable={true}/>
+    <Test label="clinical pathology" links={HISTO} downloadable={true}/>
+    <Test label="pharmacology" links={MICRO} downloadable={true}/>
+    <Test label="microbiology" links={MICRO} downloadable={true}/>
    </div>
-    
-    </div>
+   </div>
    
     <Footer/>
      </>
